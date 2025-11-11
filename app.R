4855868e71f1eb5ea7b354374d0ab56b8babb5f8
Raphael Saldanha <rfsaldanha@gmail.com>
2025-11-11 12:35:05 -0300

    first version

--- app.R
+++ app.R
@@ -1,6 +1,35 @@
 # Packages
 library(shiny)
 library(bslib)
+library(arrow)
+library(dplyr)
+library(sf)
+library(lubridate)
+library(ggplot2)
+library(inmetrss)
+
+# INMET parquet file address
+parquet_url <- "https://inmetalerts.nyc3.cdn.digitaloceanspaces.com/inmetalerts.parquet"
+
+# INMET dataset
+ds <- read_parquet(parquet_url)
+ds <- parse_mun(ds)
+ds$mun_codes <- substr(ds$mun_codes, 0, 6)
+
+# Read municipality seats data
+mun_seats <- readRDS("data/mun_seats.rds")
+
+# Municipality list for selector
+mun_names <- mun_seats$code_muni
+names(mun_names) <- paste(mun_seats$name_muni, "-", mun_seats$abbrev_state)
+
+# Municipality codes and names
+ref_mun_names <- mun_seats |>
+  st_drop_geometry() |>
+  select(code_muni, name_muni, abbrev_state) |>
+  mutate(name_muni = paste(name_muni, "-", abbrev_state)) |>
+  select(-abbrev_state) |>
+  as_tibble()
 
 # Interface
 ui <- page_navbar(
@@ -59,64 +88,92 @@ ui <- page_navbar(
 
   # Map page
   nav_panel(
-    title = "Página A",
+    title = "Alertas meteorológicos",
 
     # Sidebar
     layout_sidebar(
-      sidebar = sidebar(),
+      sidebar = sidebar(
+        uiOutput(outputId = "sel_year_UI"),
+        uiOutput(outputId = "sel_mun_UI")
+      ),
 
       # Card
       card(
         full_screen = TRUE,
         card_body(
-          class = "p-0" # Fill card, used for maps
+          plotOutput(outputId = "graph")
         )
-      )
-    )
-  ),
-
-  # Graphs page
-  nav_panel(
-    title = "Página B",
-
-    layout_sidebar(
-      sidebar = sidebar(),
-
-      # Graphs card
-      card(
-        full_screen = TRUE,
-        card_header("Card header"),
-        card_body()
       )
     )
   ),
 
   # About page
   nav_panel(
-    title = "Página B",
+    title = "Sobre o projeto",
     card(
-      card_header("Card title"),
-      p("Bla bla bla.")
-    ),
-    accordion(
-      multiple = FALSE,
-      accordion_panel(
-        "Título A",
-        p("Bla bla bla.")
-      ),
-      accordion_panel(
-        "Título B",
-        p("Bla bla bla.")
-      ),
-      accordion_panel(
-        "Título C",
-        p("Bla bla bla.")
+      p(
+        "Acompanhamento de dados de alertas meteorológicos coletados diariamente no sistema de alertas do INMET."
       )
     )
   )
 )
 
 # Server
-server <- function(input, output, session) {}
+server <- function(input, output, session) {
+  # Year selector
+  output$sel_year_UI <- renderUI({
+    res <- ds |>
+      mutate(year = year(onset)) |>
+      select(year) |>
+      distinct(year) |>
+      arrange(-year) |>
+      pull(year)
+
+    selectInput(
+      inputId = "sel_year",
+      choices = res,
+      label = "Ano"
+    )
+  })
+
+  # Municipality selector
+  output$sel_mun_UI <- renderUI({
+    selectInput(
+      inputId = "sel_mun",
+      choices = mun_names,
+      label = "Município"
+    )
+  })
+
+  output$graph <- renderPlot({
+    req(input$sel_mun)
+
+    name <- ref_mun_names |>
+      filter(code_muni == input$sel_mun) |>
+      select(name_muni)
+
+    ds |>
+      filter(mun_codes == input$sel_mun) |>
+      mutate(year = year(onset)) |>
+      filter(year == 2025) |>
+      ggplot(aes(
+        x = onset,
+        xend = expires,
+        y = event,
+        yend = event,
+        col = event
+      )) +
+      geom_segment(size = 20) +
+      labs(
+        title = "Alertas meteorológicos INMET",
+        subtitle = name$name_muni[1],
+        x = "Duração",
+        y = NULL,
+        caption = "Elaboração: LIS/ICICT/Fiocruz"
+      ) +
+      theme_bw() +
+      theme(legend.position = "none")
+  })
+}
 
 shinyApp(ui, server)
