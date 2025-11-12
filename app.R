c9520a14aca1ce309a3b7c98d490fba976c4335c
Raphael Saldanha <rfsaldanha@gmail.com>
2025-11-12 15:28:57 -0300

    modes

--- app.R
+++ app.R
@@ -9,12 +9,17 @@ library(lubridate)
 library(ggplot2)
 library(inmetrss)
 library(DT)
+library(tmap)
+library(shinybusy)
+busy_bar_color <- "brown"
+busy_bar_height <- "4px"
 
 # INMET parquet file address
 parquet_url <- "https://inmetalerts.nyc3.cdn.digitaloceanspaces.com/inmetalerts.parquet"
 
 # INMET dataset
-ds <- read_parquet(parquet_url)
+ds <- read_parquet(parquet_url) |>
+  distinct()
 ds <- parse_mun(ds)
 
 # Read municipality seats data
@@ -23,6 +28,9 @@ mun <- readRDS("data/mun.rds")
 # Municipality list for selector
 mun_names <- mun$code_muni
 names(mun_names) <- mun$name_muni
+
+# Read municipality polygon geometry
+br.shp <- readRDS("data/br_mun_2024.rds")
 
 # Interface
 ui <- page_navbar(
@@ -84,14 +92,15 @@ ui <- page_navbar(
     )
   ),
 
-  # Map page
+  # Graph page
   nav_panel(
     title = "Alertas meteorológicos",
 
     card(
       full_screen = TRUE,
       card_body(
-        plotOutput(outputId = "graph")
+        plotOutput(outputId = "graph"),
+        add_busy_bar(color = busy_bar_color, height = busy_bar_height)
       )
     )
   ),
@@ -103,12 +112,32 @@ ui <- page_navbar(
     accordion(
       multiple = FALSE,
       accordion_panel(
+        title = "Alertas mais recentes",
+        dataTableOutput(outputId = "table_recent"),
+        add_busy_bar(color = busy_bar_color, height = busy_bar_height)
+      ),
+      accordion_panel(
         title = "Quantidade de alertas",
-        dataTableOutput(outputId = "table_qtd")
+        dataTableOutput(outputId = "table_qtd"),
+        add_busy_bar(color = busy_bar_color, height = busy_bar_height)
       ),
       accordion_panel(
         title = "Duração de alertas (dias)",
-        dataTableOutput(outputId = "table_duration")
+        dataTableOutput(outputId = "table_duration"),
+        add_busy_bar(color = busy_bar_color, height = busy_bar_height)
+      )
+    )
+  ),
+
+  # Map page
+  nav_panel(
+    title = "Mapa",
+
+    card(
+      full_screen = TRUE,
+      card_body(
+        tmapOutput(outputId = "map"),
+        add_busy_bar(color = busy_bar_color, height = busy_bar_height)
       )
     )
   ),
@@ -171,7 +200,10 @@ server <- function(input, output, session) {
       )) +
       geom_segment(size = 25) +
       labs(
-        title = "Alertas meteorológicos emitidos pelo INMET",
+        title = paste(
+          "Alertas meteorológicos emitidos pelo INMET -",
+          input$sel_year
+        ),
         subtitle = name$name_muni[1],
         x = "Duração",
         y = NULL,
@@ -179,6 +211,23 @@ server <- function(input, output, session) {
       ) +
       theme_bw() +
       theme(legend.position = "none", text = element_text(size = 12))
+  })
+
+  output$table_recent <- renderDT({
+    res <- ds |>
+      mutate(sent = as_datetime(sent)) |>
+      group_by(identifier, event, sent) |>
+      summarise(freq = n()) |>
+      ungroup() |>
+      arrange(desc(sent)) |>
+      rename(
+        `Identificador` = identifier,
+        `Evento` = event,
+        `Envio` = sent,
+        `Municípios afetados` = freq
+      )
+
+    datatable(data = res, rownames = FALSE, options = list(dom = 'ftp'))
   })
 
   output$table_qtd <- renderDT({
@@ -222,6 +271,38 @@ server <- function(input, output, session) {
       rename(`Ano` = year, `Município` = name_muni)
 
     datatable(data = res, rownames = FALSE, options = list(dom = 'ftp'))
+  })
+
+  output$map <- renderTmap({
+    req(input$sel_year)
+
+    res <- ds |>
+      mutate(year = year(onset)) |>
+      filter(year == input$sel_year) |>
+      mutate(mun_codes = as.numeric(mun_codes)) |>
+      group_by(year, mun_codes, event) |>
+      summarise(freq = n()) |>
+      ungroup() |>
+      pivot_wider(names_from = event, values_from = freq) |>
+      mutate(across(everything(), ~ replace_na(.x, 0))) |>
+      pivot_longer(cols = 3:9)
+
+    res <- left_join(br.shp, res, by = c("code_muni" = "mun_codes")) |>
+      na.omit()
+
+    tm_shape(res) +
+      tm_title(paste(
+        "Alertas meteorológicos emitidos pelo INMET -",
+        input$sel_year
+      )) +
+      tm_polygons(
+        fill = "value",
+        lwd = 0,
+        fill.free = TRUE,
+        fill.legend = tm_legend(show = FALSE),
+        fill.scale = tm_scale_intervals(values = "brewer.or_rd")
+      ) +
+      tm_facets(by = "name")
   })
 }
 
